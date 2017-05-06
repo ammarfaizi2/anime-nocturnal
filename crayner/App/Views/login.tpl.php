fe9f73f12ac72e66939f556deebd0325e19a3e92
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-06 22:29:57 +0700

    Fix og:image

--- crayner/App/Views/login.tpl.php
+++ crayner/App/Views/login.tpl.php
@@ -10,6 +10,7 @@ if (isset($_COOKIE['alert'])) {
 	<title>RedAngel Login</title>
 	<meta charset="UTF-8">
 	<meta property="og:title" content="Login">
+	<meta property="og:image" content="<?php print base_url().'/assets/img/bgm/1.jpg'; ?>">
 	<meta property="og:description" content="RedAngel Login">
   	<meta name="description" content="Login">
   	<meta name="keywords" content="RedAngel">
@@ -27,6 +28,7 @@ if (isset($_COOKIE['alert'])) {
 			background-image: <?php print 'url('.base_url().'/assets/img/bgm/1.jpg);';?>
 			background-color: #cccccc;
 			background-repeat: no-repeat;
+			background-attachment: fixed;
 			background-position: top; 
 		}
 	</style>
