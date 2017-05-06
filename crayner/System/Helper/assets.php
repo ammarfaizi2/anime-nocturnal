ea64e21aa95c50853962496db730e20977e06425
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-06 17:14:49 +0700

    Add breakline in assets helper

--- crayner/System/Helper/assets.php
+++ crayner/System/Helper/assets.php
@@ -4,12 +4,12 @@ use System\ConfigHandler;
 if (!function_exists('js')) {
     function js($file, $abs=false)
     {
-        print '<script type="text/javascript" src="'.(ConfigHandler::iq()->assets('js')).'/'.($abs?$file:$file.'.js').'">';
+        print '<script type="text/javascript" src="'.(ConfigHandler::iq()->assets('js')).'/'.($abs?$file:$file.'.js').'">'."\n";
     }
 }
 if (!function_exists('css')) {
     function css($file, $abs=false)
     {
-        print '<link rel="stylesheet" type="text/css" href="'.(ConfigHandler::iq()->assets('css')).'/'.($abs?$file:$file.'.css').'">';
+        print '<link rel="stylesheet" type="text/css" href="'.(ConfigHandler::iq()->assets('css')).'/'.($abs?$file:$file.'.css').'">'."\n";
     }
 }
