95c18eb26f26bc2a6f0791063ef41ebc7787dae3
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-06 15:37:00 +0700

    Login Controller

---
+++ crayner/App/Controllers/login.php
@@ -0,0 +1,66 @@
+<?php
+namespace App\Controllers;
+
+use System\Controller;
+/**
+*
+*		Created by icetea
+*
+*/
+class login extends Controller
+{
+	/**
+	* Controller constructor
+	*/
+	public function __construct()
+	{
+		parent::__construct();
+        $this->load->helper('url');
+        $this->load->helper('rstr');
+        $this->load->helper('teacrypt');
+        $this->load->helper('cookiemgr');
+	}
+	
+	/**
+	* Default method
+	*/
+	public function index()
+	{
+		$login = new \App\Models\Login();
+        if ($login->login_status()) {
+            header('location:'.base_url().'/home?ref=login');
+        } else {
+        	if (isset($_POST['login'])) {
+        		if($login->check_token())	{
+        			if($login->check_login()){
+        				
+        			}
+        		} else {
+        			$this->error_token();
+        		}
+        		die;
+        	}
+            $this->load->view('login', array('token'=>$login->token()));
+        }
+	}
+
+
+
+
+
+	private function error_token()
+	{
+		header('location:'.base_url().'/login?ref=err_token');
+		?>
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Error Token !</title>
+		</head>
+		<body>
+			<h1>Error Token !</h1>
+		</body>
+		</html>
+		<?php
+	}
+}
