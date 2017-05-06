9d4765d03989ea30865db1278aec826f00463a26
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-06 14:52:07 +0700

    Setting database

--- crayner/System/Helper/rstr.php
+++ crayner/System/Helper/rstr.php
@@ -2,7 +2,7 @@
 if (!function_exists('rstr')) {
     function rstr($n=32, $concat='')
     {
-        $a = "1234567890qwertyuiopasdfghjklzxcvbnm__".$concat;
+        $a = "1234567890qwertyuiopasdfghjklzxcvbnm____".$concat;
         $s = strlen($a)-1;
         $rt = '';
         for ($i=0; $i < $n; $i++) {
