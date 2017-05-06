d465f314864639a4c7d309016f346f41aec4929f
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-06 18:49:43 +0700

    Fix generate_userid

--- crayner/App/Models/Register.php
+++ crayner/App/Models/Register.php
@@ -87,7 +87,7 @@ class Register extends Model
     {
         $st = $this->db->prepare("SELECT COUNT(`userid`) FROM `account_data` WHERE `userid`=:userid LIMIT 1;");
         do {
-            $userid = rstr(16, null, '1234567890');
+            $userid = rand(1,9).rstr(15, null, '1234567890');
             $st->execute(array(':userid'=>$userid));
             $dt = $st->fetch(\PDO::FETCH_NUM);
         } while ((bool)$dt[0]);
