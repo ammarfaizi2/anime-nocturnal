fc6cea33954ccca70243d5461981bb441029e791
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-09 16:50:17 +0700

    Create profile controller

--- crayner/System/Helper/cookiemgr.php
+++ crayner/System/Helper/cookiemgr.php
@@ -3,7 +3,7 @@ if (!function_exists('stck')) {
     function stck($ck)
     {
         $domain = (strpos($_SERVER['HTTP_HOST'], 'localhost')===false) ? $_SERVER['HTTP_HOST'] : false;
-        $https = isset($_SERVER['HTTPS']);
+        $https = isset($_SERVER['HTTPS']) ? true : null;
         $now = time();
         foreach ($ck as $key => $val) {
             $st[] = setcookie($key, $val[0], $now+(int)(isset($val[1])?$val[1]:3600), '/', $domain, $https);
@@ -15,7 +15,7 @@ if (!function_exists('rmck')) {
     function rmck($ck)
     {
         $domain = (strpos($_SERVER['HTTP_HOST'], 'localhost')===false) ? $_SERVER['HTTP_HOST'] : false;
-        $https = isset($_SERVER['HTTPS']);
+        $https = isset($_SERVER['HTTPS']) ? true : null;
         foreach ($ck as $val) {
             $st[] = setcookie($val, null, 0, '/', $domain, $https);
         }
