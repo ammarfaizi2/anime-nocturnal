325269028aba4d74e4413398502a327151d340ca
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-09 22:11:37 +0700

    Fix profile view

--- crayner/App/Views/profile.tpl.php
+++ crayner/App/Views/profile.tpl.php
@@ -13,7 +13,7 @@ $l->load->view('static/header', array(
 <center>
 <div class="prf">
 	<img src="<?php print !empty($u['photo']) ? $u['photo'] : '';?>">
-	<form id="myForm" action="<?php print htmlspecialchars(base_url().'/upload/photo?ref=profile&wg='.rstr(32),ENT_QUOTES,'UTF-8'); ?>" method="get">
+	<form id="myForm" action="<?php print htmlspecialchars(base_url().'/upload/photo?ref=profile&wg='.rstr(32)."&td=".md5(rstr(5)),ENT_QUOTES,'UTF-8'); ?>" method="get">
 	<input type="file" name="photo" accept="image/*" onchange="__preview(this,'preview')" />
 	<input type="submit" value="Upload" /><br/>
 	<img id="preview" src="" alt="" width="320px"/>
